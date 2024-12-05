fa83357b4ef27d57e11801cccc59c619df453e79
diorm47 <dior47m@gmail.com>
2024-12-05 21:57:54 +0500

    initial deploy

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+function setSuccessBlock() {
+  document.querySelector('.success_contact_form').classList.remove('hidden')
+  document.querySelector('.contacts_us_form_form').classList.add('hidden')
+}
